e197974d8bc74abb5a6cb0365d80428d695c74fc
akihiro-hosoya <iicina.yftj.33@gmail.com>
2020-04-19 14:48:59 +0900

    URL

--- foodie/admin.py
+++ foodie/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Pref
 
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_l', 'name')
+    list_display_links = ('category_l',)
+    list_editable = ('name',)
+
+@admin.register(Pref)
+class PrefAdmin(admin.ModelAdmin):
+    list_display = ('pref', 'name') # カラム表示したいフィールドの名前をタプルにして指定
+    list_display_links = ('pref',)
+    list_editable = ('name',)
+
